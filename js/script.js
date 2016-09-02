5399a5fb7d846c34c8bde911465cff087020e2b1
marina-rusakova <marinarusakova111@gmail.com>
2016-09-02 06:15:18 -0700

    Проект на финальную проверку (правки)

--- js/script.js
+++ js/script.js
@@ -8,34 +8,34 @@
   link.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.add("modal-content-show");
-     if (storage) {
-       login.value = storage;
-       password.focus
-     } else {
-       login.focus();
-     }
+      if (storage) {
+        login.value = storage;
+        password.focus
+      } else {
+        login.focus();
+      }
    });
 
   close.addEventListener("click", function(event) {
-     event.preventDefault();
-     popup.classList.remove("modal-content-show");
-     popup.classList.remove("modal-error");
+    event.preventDefault();
+    popup.classList.remove("modal-content-show");
+    popup.classList.remove("modal-error");
    });
 
   form.addEventListener("submit", function(event) {
-     if (!login.value || !mail.value) {
-       event.preventDefault();
-       popup.classList.remove("modal-error");
-       popup.offsetWidth = popup.offsetWidth;
-       popup.classList.add("modal-error");
-        } else {
-       localStorage.setItem("login", login.value);
+    if (!login.value || !mail.value) {
+      event.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    } else {
+      localStorage.setItem("login", login.value);
      } 
    });
   window.addEventListener("keydown", function(event) {
-     if (event.keyCode === 27) {
-       if (popup.classList.contains("modal-content-show")) {
-         popup.classList.remove("modal-content-show");
+    if (event.keyCode === 27) {
+      if (popup.classList.contains("modal-content-show")) {
+        popup.classList.remove("modal-content-show");
         popup.classList.remove("modal-error");
        }
      }
